bdb3365c9d6bd665c2a2feecb491457e98f19cf6
Vipul Babhare <vipul.babhare@inspirext.com>
2025-07-16 22:32:20 +0530

    🔥 Fresh repo init without .terraform folder

---
+++ README.md
@@ -0,0 +1,1 @@
+# azure-vm-auto-python
---
+++ main.tf
@@ -0,0 +1,110 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "8d744b3b-0800-4ca7-bc56-eb983321bb28" # your subscription
+  use_cli         = true
+}
+
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "test-vm-group"
+  location = "UK South"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "test-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "test-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Network Security Group (NSG)
+resource "azurerm_network_security_group" "example" {
+  name                = "test-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Public IP
+resource "azurerm_public_ip" "example" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = "myuniquednslabel1234" # must be globally unique
+}
+
+# Network Interface
+resource "azurerm_network_interface" "example" {
+  name                = "test-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Associate NSG to NIC
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "test-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# Optional Output (to get IP)
+output "public_ip" {
+  value = azurerm_public_ip.example.ip_address
+}
