52b5be3000a71b219f3e565b615fc7276f9efd97
Vipul Babhare <vipul.babhare@inspirext.com>
2025-07-17 14:42:13 +0530

    ✅ Added deploy workflow correctly

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/id_rsa_azure.pub")
   }
 
   os_disk {
