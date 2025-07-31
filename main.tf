9d3f2b56ed202497bdc1a419ef1b0a2c99930f9d
Vipul Babhare <vipul.babhare@inspirext.com>
2025-07-31 14:48:17 +0530

    🔓 Opened port 8501 for TensorFlow Serving

--- main.tf
+++ main.tf
@@ -1,11 +1,47 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "ab16a6e7-fa55-4581-b4dd-5602b818cd8e"
+  use_cli         = true
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH Public Key for the VM"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "test-vm-groupterraf"
+  location = "UK South"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "test-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "test-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_network_security_group" "example" {
   name                = "test-nsg"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
-  # Allow SSH
+  # Allow SSH (port 22)
   security_rule {
-    name                       = "SSH"
+    name                       = "Allow-SSH"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
@@ -28,4 +64,65 @@ resource "azurerm_network_security_group" "example" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = "vipulvm-${random_string.suffix.result}"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "test-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "test-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+output "public_ip" {
+  value       = azurerm_public_ip.example.ip_address
+  description = "Public IP of the Azure VM"
+  sensitive   = false
 }
