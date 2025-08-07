89519f384d99ea5f64796e80c586f0a450fd3cf7
Vipul Babhare <vipul.babhare@inspirext.com>
2025-08-07 16:59:26 +0530

    🔁 Rename jwt-auth folder, update validator, cleanup structure

--- infra/pipeline2/main.tf
+++ infra/pipeline2/main.tf
@@ -21,49 +21,43 @@ variable "allowed_ip_for_tf_serving" {
   default     = "0.0.0.0/0"
 }
 
-# Reuse resource group created by pipeline1
+# Reference existing Resource Group
 data "azurerm_resource_group" "example" {
   name = "test-vm-groupterraf"
 }
 
-# Reuse existing virtual network
+# Reference existing Virtual Network
 data "azurerm_virtual_network" "example" {
   name                = "test-vnet"
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-# Reuse existing subnet
+# Reference existing Subnet
 data "azurerm_subnet" "example" {
   name                 = "test-subnet"
   virtual_network_name = data.azurerm_virtual_network.example.name
   resource_group_name  = data.azurerm_resource_group.example.name
 }
 
-# Reuse existing network security group
+# Reference existing NSG
 data "azurerm_network_security_group" "example" {
   name                = "test-nsg"
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-# Reuse existing public IP
+# Reference existing Public IP
 data "azurerm_public_ip" "example" {
   name                = "myPublicIP"
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-# Reuse existing NIC
+# Reference existing NIC
 data "azurerm_network_interface" "example" {
   name                = "test-nic"
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-# Re-attach NSG to NIC to be safe
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = data.azurerm_network_interface.example.id
-  network_security_group_id = data.azurerm_network_security_group.example.id
-}
-
-# Create only VM
+# Only create VM here
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "test-vm"
   resource_group_name = data.azurerm_resource_group.example.name
@@ -92,7 +86,6 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 }
 
-# Output reused IP address
 output "public_ip" {
   value       = data.azurerm_public_ip.example.ip_address
   description = "Public IP of the Azure VM"
