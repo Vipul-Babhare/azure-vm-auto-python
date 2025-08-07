d4bcc04271df242730bba531b5fdf2286e8d7894
Vipul Babhare <vipul.babhare@inspirext.com>
2025-08-07 16:54:26 +0530

    🔁 Rename jwt-auth folder, update validator, cleanup structure

--- infra/pipeline2/main.tf
+++ infra/pipeline2/main.tf
@@ -21,155 +21,49 @@ variable "allowed_ip_for_tf_serving" {
   default     = "0.0.0.0/0"
 }
 
+# Reuse resource group created by pipeline1
 data "azurerm_resource_group" "example" {
   name = "test-vm-groupterraf"
 }
 
+# Reuse existing virtual network
+data "azurerm_virtual_network" "example" {
+  name                = "test-vnet"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+# Reuse existing subnet
+data "azurerm_subnet" "example" {
+  name                 = "test-subnet"
+  virtual_network_name = data.azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+}
+
+# Reuse existing network security group
+data "azurerm_network_security_group" "example" {
+  name                = "test-nsg"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+# Reuse existing public IP
 data "azurerm_public_ip" "example" {
   name                = "myPublicIP"
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  upper   = false
-  special = false
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "test-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "test-subnet"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "test-nsg"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = var.allowed_ssh_cidr
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-TFServing"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8501"
-    source_address_prefix      = var.allowed_ip_for_tf_serving
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-Prometheus"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "9090"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-NodeExporter"
-    priority                   = 1004
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "9100"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-cAdvisor"
-    priority                   = 1005
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "9323"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-Grafana"
-    priority                   = 1006
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3000"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-TokenGenerator"
-    priority                   = 1007
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8082"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-TokenValidator"
-    priority                   = 1008
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8083"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface" "example" {
+# Reuse existing NIC
+data "azurerm_network_interface" "example" {
   name                = "test-nic"
-  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = data.azurerm_public_ip.example.id
-  }
 }
 
+# Re-attach NSG to NIC to be safe
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
+  network_interface_id      = data.azurerm_network_interface.example.id
+  network_security_group_id = data.azurerm_network_security_group.example.id
 }
 
+# Create only VM
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "test-vm"
   resource_group_name = data.azurerm_resource_group.example.name
@@ -177,7 +71,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   size                = "Standard_B1s"
   admin_username      = "azureuser"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    data.azurerm_network_interface.example.id,
   ]
 
   admin_ssh_key {
@@ -198,6 +92,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 }
 
+# Output reused IP address
 output "public_ip" {
   value       = data.azurerm_public_ip.example.ip_address
   description = "Public IP of the Azure VM"
